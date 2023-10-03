9590869c22827bff8981d2d73e656259720d76e8
filipi <filipisee@hotmail.com>
2023-10-02 22:50:05 -0300

    configured clerk && login/signin pages

--- nextlink/app/layout.tsx
+++ nextlink/app/layout.tsx
@@ -1,22 +1,26 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Open_Sans } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const open_sans = Open_Sans({ subsets: ['latin'] })
+const open_sans = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Next Talk',
-  description: 'An online platform that enables you to virtually connect with friends, teammates and communities',
-}
+  title: "Next Talk",
+  description:
+    "An online platform that enables you to virtually connect with friends, teammates and communities",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={open_sans.className}>{children}</body>
-    </html>
-  )
+    <ClerkProvider>
+      <html lang="en">
+        <body className={open_sans.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
 }
