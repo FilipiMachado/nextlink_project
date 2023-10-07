814ba68aadac9d143595dc25578a0198bd39c9c0
filipi <filipisee@hotmail.com>
2023-10-06 21:44:43 -0300

    working on file upload component

---
+++ nextlink/components/file-upload.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+const FileUpload = () => {
+  return (
+    <div>FileUpload</div>
+  )
+}
+
+export default FileUpload
--- nextlink/components/modals/initial-modal.tsx
+++ nextlink/components/modals/initial-modal.tsx
@@ -23,6 +23,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import FileUpload from "@/components/file-upload";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -76,7 +77,17 @@ const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-                TODO: Image Upload
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
               </div>
 
               <FormField
@@ -100,7 +111,7 @@ const InitialModal = () => {
                 )}
               />
             </div>
-            
+
             <DialogFooter className="px-6 py-4">
               <Button disabled={isLoading} variant="primary">
                 Create
