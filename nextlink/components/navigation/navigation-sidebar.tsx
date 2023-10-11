a0d3b56b302808dce01f7238110edff5c78b66c6
filipi <filipisee@hotmail.com>
2023-10-11 19:53:12 -0300

    working navigation sidebar

---
+++ nextlink/components/navigation/navigation-sidebar.tsx
@@ -0,0 +1,34 @@
+import { redirect } from "next/navigation";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+const NavigationSidebar = async () => {
+  const profile = await currentProfile();
+
+  //console.log(profile);
+
+  if (!profile) {
+    return redirect("/");
+  }
+
+  const servers = await db.server.findMany({
+    where: {
+      Member: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  console.log(servers);
+
+  return (
+    <div className="space-y-4 flex flex-col items-center h-full text-primary w-full py-3 dark:bg-[#1E1F22]">
+      <div>NavigationSidebar</div>
+    </div>
+  );
+};
+
+export default NavigationSidebar;
