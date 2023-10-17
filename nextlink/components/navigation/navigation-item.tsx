b8d13db9d74fde0f7a3a45e6aac6f45df7f5b7a5
filipi <filipisee@hotmail.com>
2023-10-17 20:57:49 -0300

    configuring navigation sidebar

--- nextlink/components/navigation/navigation-item.tsx
+++ nextlink/components/navigation/navigation-item.tsx
@@ -13,14 +13,17 @@ interface NavigationItemProps {
 }
 
 export const NavigationItem = ({ id, imageUrl, name }: NavigationItemProps) => {
-
-    console.log(imageUrl)
+  console.log(imageUrl);
   const params = useParams();
   const router = useRouter();
 
+  const onClick = () => {
+    router.push(`/servers/${id}`);
+  };
+
   return (
     <ActionTooltip side="right" align="center" label={name}>
-      <button onClick={() => {}} className="group relative flex items-center">
+      <button onClick={onClick} className="group relative flex items-center">
         <div
           className={cn(
             "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
--- nextlink/components/navigation/navigation-sidebar.tsx
+++ nextlink/components/navigation/navigation-sidebar.tsx
@@ -1,8 +1,10 @@
 import NavigationAction from "@/components/navigation/navigation-action";
 import { NavigationItem } from "@/components/navigation/navigation-item";
+import { ModeToggle } from "@/components/mode-toggle";
 
 import { Separator } from "@/components/ui/separator";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { UserButton } from "@clerk/nextjs";
 
 import { redirect } from "next/navigation";
 
@@ -45,6 +47,17 @@ const NavigationSidebar = async () => {
           </div>
         ))}
       </ScrollArea>
+      <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+        <ModeToggle />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: "h-[48px] w-[48px]",
+            },
+          }}
+        />
+      </div>
     </div>
   );
 };
