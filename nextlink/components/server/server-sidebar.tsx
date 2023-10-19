f9fced2d46bbd7053a09fca7539c14dd48bcd3fc
filipi <filipisee@hotmail.com>
2023-10-18 22:07:55 -0300

    structuring server sidebar

--- nextlink/components/server/server-sidebar.tsx
+++ nextlink/components/server/server-sidebar.tsx
@@ -1,7 +1,63 @@
+import { redirect } from "next/navigation";
+import { ChannelType } from "@prisma/client";
 
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
 
-export const ServerSidebar = () => {
-  return (
-    <div>ServerSidebar</div>
-  )
+interface ServerSidebarProps {
+  serverId: string;
 }
+
+export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirect("/");
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: serverId,
+    },
+    include: {
+      Channel: {
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+      Member: {
+        include: {
+          profile: true,
+        },
+        orderBy: {
+          role: "asc",
+        },
+      },
+    },
+  });
+
+  const textChannels = server?.Channel.filter(
+    (channel) => channel.type === ChannelType.TEXT
+  );
+  const audioChannels = server?.Channel.filter(
+    (channel) => channel.type === ChannelType.AUDIO
+  );
+  const videohannels = server?.Channel.filter(
+    (channel) => channel.type === ChannelType.VIDEO
+  );
+  const members = server?.Member.filter(
+    (member) => member.profileId !== profile.id
+  );
+
+  if (!server) {
+    return redirect("/");
+  }
+
+  const role = server.Member.find(
+    (member) => member.profileId === profile.id
+  )?.role;
+
+  console.log(role);
+
+  return <div>ServerSidebar</div>;
+};
