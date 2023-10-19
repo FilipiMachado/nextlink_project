a6245e62a46cccf9c33cf9a670393f034daee81a
filipi <filipisee@hotmail.com>
2023-10-19 20:21:27 -0300

    working on invite people modal

--- nextlink/components/modals/invite-modal.tsx
+++ nextlink/components/modals/invite-modal.tsx
@@ -1,51 +1,40 @@
 "use client";
 
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import axios from "axios";
-
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
+import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import FileUpload from "@/components/file-upload";
-import { useRouter } from "next/navigation";
+
 import { useModal } from "@/hooks/use-modal-store";
 
-export const CreateServerModal = () => {
+export const InviteModal = () => {
   const { isOpen, onClose, type } = useModal();
-  const router = useRouter();
 
-  const isModalOpen = isOpen && type === "createServer";
+  const isModalOpen = isOpen && type === "invite";
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Customize your server
+            Invite Friends
           </DialogTitle>
-          <DialogDescription className="text-center text-zinc-500">
-            Give your server a personality with a name and a image. You can
-            always change it later.
-          </DialogDescription>
         </DialogHeader>
-        Invite Modal
+        <div className="p-6">
+          <Label className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+            Server invite link
+          </Label>
+          <div className="flex items-center mt-2 gap-x-2">
+            <Input
+              className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+              value="invite-link"
+            />
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   );
