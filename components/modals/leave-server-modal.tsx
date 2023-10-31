acc1efd39ff785bbf35255fb0d08f2d0994b18e8
filipi <filipisee@hotmail.com>
2023-10-31 10:58:22 -0300

    working on guest leave server modal

---
+++ components/modals/leave-server-modal.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useState } from "react";
+import axios from "axios";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+import { useModal } from "@/hooks/use-modal-store";
+export const LeaveServerModal = () => {
+  const { onOpen, isOpen, onClose, type, data } = useModal();
+
+  const isModalOpen = isOpen && type === "leaveServer";
+  const { server } = data;
+
+  const [copied, setCopied] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Leave Server
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Are you sure you want do leave{" "}
+            <span className="font-semibold text-indigo-500">
+              {server?.name}
+            </span>
+            ?
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="bg-gray-100 px-6 py-4">
+          1
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
