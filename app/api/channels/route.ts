f111724de7dcb17191ae82734eb0886c000e1392
filipi <filipisee@hotmail.com>
2023-10-31 10:38:04 -0300

    finished create channel feature

--- app/api/channels/route.ts
+++ app/api/channels/route.ts
@@ -1,5 +1,8 @@
-import { currentProfile } from "@/lib/current-profile";
+import { MemberRole } from "@prisma/client";
 import { NextResponse } from "next/server";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
 
 export async function POST(req: Request) {
   try {
@@ -12,6 +15,39 @@ export async function POST(req: Request) {
     if (!profile) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
+
+    if (!serverId) {
+      return new NextResponse("Server ID missing", { status: 400 });
+    }
+
+    if (name === "general") {
+      return new NextResponse(`Name cannot be "general"`, { status: 400 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        Member: {
+          some: {
+            profileId: profile.id,
+            role: {
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR],
+            },
+          },
+        },
+      },
+      data: {
+        Channel: {
+          create: {
+            profileId: profile.id,
+            name,
+            type,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(server);
   } catch (error) {
     console.log("CHANNELS_POST", error);
     return new NextResponse("Internal Server Error", { status: 500 });
