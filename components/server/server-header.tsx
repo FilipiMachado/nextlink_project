3c2dbd497a9e83e09aa43955073ca45e94399e0c
filipi <filipisee@hotmail.com>
2023-10-31 22:34:52 -0300

    structuring delete server modal

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -79,7 +79,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         )}
         {isModerator && <DropdownMenuSeparator />}
         {isAdmin && (
-          <DropdownMenuItem className="text-rose-600 dark:text-rose-400 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("deleteServer", { server })}
+            className="text-rose-600 dark:text-rose-400 px-3 py-2 text-sm cursor-pointer"
+          >
             Delete Server
             <Trash className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
