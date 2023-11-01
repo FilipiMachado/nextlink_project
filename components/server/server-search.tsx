655936980bd849418d7e198dd7a31a4b03e8e054
filipi <filipisee@hotmail.com>
2023-11-01 20:22:32 -0300

    structuring server search component

---
+++ components/server/server-search.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+interface ServerSearchProps {
+  data: {
+    label: string;
+    type: "channel" | "member";
+    data:
+      | {
+          icon: React.ReactNode;
+          name: string;
+          id: string;
+        }[]
+      | undefined;
+  }[];
+}
+
+export const ServerSearch = ({ data }: ServerSearchProps) => {
+  return (
+    <div>
+      <div>Server Search</div>
+    </div>
+  );
+};
--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,14 +1,31 @@
+import { ChannelType, MemberRole } from "@prisma/client";
 import { redirect } from "next/navigation";
-import { ChannelType } from "@prisma/client";
+import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from "lucide-react";
 
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { ServerHeader } from "@/components/server/server-header";
+import { ServerSearch } from "@/components/server/server-search";
 
 interface ServerSidebarProps {
   serverId: string;
 }
+
+const iconMap = {
+  [ChannelType.TEXT]: <Hash className="mr-2 h-2 w-4" />,
+  [ChannelType.AUDIO]: <Mic className="mr-2 h-2 w-4" />,
+  [ChannelType.VIDEO]: <Video className="mr-2 h-2 w-4" />,
+};
+
+const roleIconMap = {
+  [MemberRole.GUEST]: null,
+  [MemberRole.MODERATOR]: (
+    <ShieldCheck className="mr-2 h-2 w-4 text-indigo-500" />
+  ),
+  [MemberRole.ADMIN]: <ShieldAlert className="mr-2 h-2 w-4 text-rose-500" />,
+};
 
 export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
   const profile = await currentProfile();
@@ -44,7 +61,7 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
   const audioChannels = server?.Channel.filter(
     (channel) => channel.type === ChannelType.AUDIO
   );
-  const videohannels = server?.Channel.filter(
+  const videoChannels = server?.Channel.filter(
     (channel) => channel.type === ChannelType.VIDEO
   );
   const members = server?.Member.filter(
@@ -67,6 +84,48 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
   return (
     <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
       <ServerHeader server={serverWithMembers} role={role} />
+      <ScrollArea className="mt-2">
+        <ServerSearch
+          data={[
+            {
+              label: "Text Channels",
+              type: "channel",
+              data: textChannels?.map((channel) => ({
+                id: channel.id,
+                name: channel.name,
+                icon: iconMap[channel.type],
+              })),
+            },
+            {
+              label: "Voice Channels",
+              type: "channel",
+              data: audioChannels?.map((channel) => ({
+                id: channel.id,
+                name: channel.name,
+                icon: iconMap[channel.type],
+              })),
+            },
+            {
+              label: "Video Channels",
+              type: "channel",
+              data: videoChannels?.map((channel) => ({
+                id: channel.id,
+                name: channel.name,
+                icon: iconMap[channel.type],
+              })),
+            },
+            {
+              label: "Members",
+              type: "member",
+              data: members?.map((member) => ({
+                id: member.id,
+                name: member.profile.name,
+                icon: roleIconMap[member.role],
+              })),
+            },
+          ]}
+        />
+      </ScrollArea>
     </div>
   );
 };
