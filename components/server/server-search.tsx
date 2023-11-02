c4fce28a8b863bae9b78094cd900ddd718c92e9f
filipi <filipisee@hotmail.com>
2023-11-01 23:03:11 -0300

    working on search channels and members feature

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -1,4 +1,15 @@
 "use client";
+import { useState } from "react";
+
+import { Search } from "lucide-react";
+
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandList,
+} from "@/components/ui/command";
 
 interface ServerSearchProps {
   data: {
@@ -15,9 +26,33 @@ interface ServerSearchProps {
 }
 
 export const ServerSearch = ({ data }: ServerSearchProps) => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div>
-      <div>Server Search</div>
-    </div>
+    <>
+      <button
+        onClick={() => setOpen(true)}
+        className="group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
+      >
+        <Search className="w-4 h-4 text-zinc-500 dark:text-zinc-400" />
+        <p className="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition">
+          Search
+        </p>
+        <kbd className="pointer-events-none inline-flex h-5 select-none items-centergap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ml-auto">
+          <span className="text-xs">CTRL</span>K
+        </kbd>
+      </button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Search all channels and members" />
+        <CommandList>
+          <CommandEmpty>No results found</CommandEmpty>
+          {data.map(({ label, type, data }) => {
+            if (!data?.length) return null;
+
+            return <CommandGroup key={label} heading={label}></CommandGroup>;
+          })}
+        </CommandList>
+      </CommandDialog>
+    </>
   );
 };
