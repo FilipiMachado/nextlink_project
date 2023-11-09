5a2a0dd86bfe231c45a34f731d9cdb565db11423
filipi <filipisee@hotmail.com>
2023-11-08 21:30:46 -0300

    structuring edit channel modal

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -9,7 +9,8 @@ import { MembersModal } from "@/components/modals/members-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 import { DeleteServerModal } from "@/components/modals/delete-server-modal";
-import { DeleteChannelModal } from "@/components/modals/delete-channel-modal copy";
+import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
+import { EditChannelModal } from "@/components/modals/edit-channel-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -32,6 +33,7 @@ export const ModalProvider = () => {
       <LeaveServerModal />
       <DeleteServerModal />
       <DeleteChannelModal />
+      <EditChannelModal />
     </>
   );
 };
