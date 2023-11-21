b14283c6b793c9d835369c75f0d39e15340e8f2e
filipi <filipisee@hotmail.com>
2023-11-21 09:34:04 -0300

    working on server page

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,7 +1,40 @@
-const ServerIdPage = () => {
-  return (
-    <div>Server ID Page</div>
-  )
+import { redirectToSignIn } from "@clerk/nextjs";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
+  };
 }
 
-export default ServerIdPage
+const ServerIdPage = async ({ params }: ServerIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      Member: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      Channel: {
+        where: {
+          name: "general",
+        },
+      },
+    },
+  });
+
+  return <div>Server ID Page</div>;
+};
+
+export default ServerIdPage;
