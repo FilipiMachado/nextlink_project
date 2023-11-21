19ac73c943d0e4714750ccc1d899f0ab709024d0
filipi <filipisee@hotmail.com>
2023-11-21 16:20:33 -0300

    structuring socket.io

---
+++ components/providers/socket-provider.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+import { io as ClientIO } from "socket.io-client";
+
+type SocketContextType = {
+  socket: any | null;
+  isConnected: boolean;
+};
+
+const SocketContext = createContext<SocketContextType>({
+  socket: null,
+  isConnected: false,
+});
+
+export const useSocket = () => {
+  return useContext(SocketContext);
+};
+
+export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
+  const [socket, setSocket] = useState(null);
+  const [isConnected, setIsConnected] = useState(false)
+
+  useEffect(() => {
+    const socketInstance = new (ClientIO as any)(process.env)
+  }, [])
+};
