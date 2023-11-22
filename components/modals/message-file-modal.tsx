bafde24ada9cc74a30eaf766626f1a08eefeac99
filipi <filipisee@hotmail.com>
2023-11-22 15:19:36 -0300

    working on message file modal component

--- components/modals/message-file-modal.tsx
+++ components/modals/message-file-modal.tsx
@@ -29,9 +29,6 @@ import FileUpload from "@/components/file-upload";
 import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
-  name: z.string().min(1, {
-    message: "Server name is required.",
-  }),
   imageUrl: z.string().min(1, {
     message: "Server image URL is required.",
   }),
@@ -75,11 +72,10 @@ const MessageFileModal = () => {
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Customize your server
+            Add an attachment
           </DialogTitle>
           <DialogDescription className="text-center text-zinc-500">
-            Give your server a personality with a name and a image. You can
-            always change it later.
+            Send a file as a message
           </DialogDescription>
         </DialogHeader>
         <Form {...form}>
@@ -93,7 +89,7 @@ const MessageFileModal = () => {
                     <FormItem>
                       <FormControl>
                         <FileUpload
-                          endpoint="serverImage"
+                          endpoint="messageFile"
                           value={field.value}
                           onChange={field.onChange}
                         />
@@ -102,32 +98,11 @@ const MessageFileModal = () => {
                   )}
                 />
               </div>
-
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
-                      Server Name
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        disabled={isLoading}
-                        className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-                        placeholder="Enter server name"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
             </div>
 
             <DialogFooter className="px-6 py-4">
               <Button disabled={isLoading} variant="primary">
-                Create
+                Send
               </Button>
             </DialogFooter>
           </form>
