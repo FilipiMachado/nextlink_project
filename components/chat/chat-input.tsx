8aff03b75886c78712a787d9d916aa00d3963d67
filipi <filipisee@hotmail.com>
2023-11-23 15:49:50 -0300

    structuring chat item component

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -47,6 +47,7 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
       await axios.post(url, values);
 
       form.reset();
+      console.log("SEND MESSAGE BUTTON")
       router.refresh()
     } catch (error) {
       console.log(error);
--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { Fragment } from "react";
+import { format } from "date-fns";
 import { Member, Message, Profile } from "@prisma/client";
 import { Loader2, ServerCrash } from "lucide-react";
 
 import { ChatWelcome } from "@/components/chat/chat-welcome";
 import { ChatItem } from "@/components/chat/chat-item";
 import { useChatQuery } from "@/hooks/use-chat-query";
+
+const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
 type MessageWithMemberWithProfile = Message & {
   member: Member & {
@@ -48,7 +51,7 @@ export const ChatMessages = ({
     });
 
   if (status === "pending") {
-    console.log("pending")
+    console.log("pending");
     return (
       <div className="flex flex-col flex-1 justify-center items-center">
         <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
@@ -78,7 +81,19 @@ export const ChatMessages = ({
         {data?.pages?.map((group, i) => (
           <Fragment key={i}>
             {group.items.map((message: MessageWithMemberWithProfile) => (
-              <ChatItem />
+              <ChatItem
+                key={message.id}
+                id={message.id}
+                currentMember={member}
+                member={message.member}
+                content={message.content}
+                fileUrl={message.fileUrl}
+                deleted={message.deleted}
+                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                isUpdated={message.updatedAt !== message.createdAt}
+                socketUrl={socketUrl}
+                socketQuery={socketQuery}
+              />
             ))}
           </Fragment>
         ))}
